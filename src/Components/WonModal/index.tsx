a243dbc80595485d683cca2644e1e598ae5e3e98
John Kornegay <91643982+johnathew@users.noreply.github.com>
2024-10-16 15:09:05 -0500

    modal accessibility update

--- src/Components/WonModal/index.tsx
+++ src/Components/WonModal/index.tsx
@@ -4,23 +4,24 @@ import styles from "./Won.module.css"
 const WonModal = forwardRef<HTMLDialogElement>(function WonModal(_, ref) {
 
     return (
-        <dialog ref={ref}>
+        <dialog ref={ref} aria-labelledby="dialog-title" aria-describedby="dialog-description">
             <form method="dialog" className={styles.dialogForm}>
-                <button>&#9932;</button>
-                <p>You did it! You completed one of the most technologically complex puzzles of this generation.
+                <button aria-label="Close">&#9932;</button>
+                <p id="dialog-title">You did it!</p>
+                <p id="dialog-description">
+                    You completed one of the most technologically complex puzzles of this generation.
                     To view the code that brought you this magnificent, beautiful, and elegant memory game please click the link below.
                 </p>
             </form>
             <div className={styles.extLinks}>
                 <a href="https://github.com/johnathew/memory_game" target="_blank">
-                    <button>Code</button>
+                    <button aria-label="View Code">Code</button>
                 </a>
                 <a href="https://jaksresume.netlify.app/" target="_blank">
-                    <button>Portfolio</button>
+                    <button aria-label="View Portfolio">Portfolio</button>
                 </a>
             </div>
         </dialog>
-
     )
 
 })
